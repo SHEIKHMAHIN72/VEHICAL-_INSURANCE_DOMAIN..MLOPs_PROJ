657febd24b60c84f4ab9c58052d0664144a8994a
SheikhMahin72 <mindsetmumentom@gmail.com>
2026-04-17 12:07:13 +0530

    1 adding files and templates

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'src',
+    version='0.0.1',
+    author='M.Sheikh',
+    author_email='mindsetmumentom@gmail',
+    packages=find_packages()
+)
